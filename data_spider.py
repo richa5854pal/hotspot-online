d662a8701771ce1e1098d402088f708a9beaec43
richa5854pal <52285563+richa5854pal@users.noreply.github.com>
2019-08-08 09:29:24 +0800

    Update data_spider.py

--- data_spider.py
+++ data_spider.py
@@ -28,12 +28,14 @@ def parse_baidu(baidu_url,fname):
     with open(fname,"w+",encoding='utf-8') as f:
         f.write(str_list) 
 
+#也可以使用https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=50&desktop=true  获取知乎热榜
+
 #知乎全站热榜
 def zhihu_rb():
     fname = os.getcwd()+"\\json\\" +"zhihu.json"
     zhihu_all = "https://www.zhihu.com/hot"
     headers = {'user-agent':'Baiduspider',
-               'cookie':'_zap=c9245bd9-4c33-4336-8ba8-5b3dd669f112; _xsrf=25zbdplpf8g1eOTx8liThpJvjXIKNFpU; d_c0="AHCjLZP2vA-PTm5R4BJliqFfV0GHoca7-pc=|1563159142"; q_c1=03225c2e9e0c496fbd456e6b19b22d2c|1563358352000|1563358352000; __utmv=51854390.100-1|2=registration_date=20161012=1^3=entry_date=20161012=1; tst=h; __utma=51854390.1006290711.1563358355.1563358355.1563540380.2; __utmz=51854390.1563540380.2.2.utmcsr=360doc.com|utmccn=(referral)|utmcmd=referral|utmcct=/content/18/0119/13/25592655_723341899.shtml; tgw_l7_route=060f637cd101836814f6c53316f73463; tshl=; capsion_ticket="2|1:0|10:1563586609|14:capsion_ticket|44:ODIwNzM4N2I0YWJjNDczMzgyNjllNmUwZjZlZGJjMjc=|54fa40fb14fc3e8fdf90a1e36c33278a6b2b872de7d9c914ea71eab9942d14b6"; z_c0="2|1:0|10:1563586611|4:z_c0|92:Mi4xQXBPUEF3QUFBQUFBY0tNdGtfYThEeVlBQUFCZ0FsVk5NNzRmWGdEaDl6NnJndHB2M3RCSlJIb3o4LTJlc0lKU05B|4c3cb58a8ea6615d1d6db372165a0199daed52f908ec02cd7665a41abe166a2b"'          
+               'cookie':'此处用你自己的cookie'          
     }
     r = requests.get(zhihu_all,headers=headers)
     r.encoding='utf-8'
